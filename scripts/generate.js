1f1db621a2bc47c42c8550795c0b901ed4ea6ce1
Ben Brandt <benjamin.j.brandt@gmail.com>
2026-07-06 14:56:58 +0200

    feat: Update schema to v1.19.0

--- scripts/generate.js
+++ scripts/generate.js
@@ -10,7 +10,7 @@ import * as fs from "fs/promises";
 import { dirname } from "path";
 import * as prettier from "prettier";
 
-const CURRENT_SCHEMA_RELEASE = "schema-v1.17.0";
+const CURRENT_SCHEMA_RELEASE = "schema-v1.19.0";
 
 await main();
 
